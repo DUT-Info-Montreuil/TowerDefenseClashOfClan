057d0ae5eb03f352de9ca858f5da45264ca6f6e6
guclu <gucluemre976@gmail.com>
2023-06-11 16:23:09 +0200

    ajout Label PvHdv

--- src/main/java/fr/iut/montreuil/projetFinal/modele/Hdv.java
+++ src/main/java/fr/iut/montreuil/projetFinal/modele/Hdv.java
@@ -1,18 +1,28 @@
 package fr.iut.montreuil.projetFinal.modele;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 public class Hdv {
     protected Environnement env;
-    private int pv;
+    private IntegerProperty pv;
     public Hdv(Environnement env){
-        pv=100;
+        pv= new SimpleIntegerProperty(100);
         this.env = env;
     }
 
-    public int getPv() {
+    public final int getPv(){
+        return pv.getValue();
+    }
+
+    public final void setPv(int n){
+        pv.setValue(n);
+    }
+    public final IntegerProperty pv(){
         return pv;
     }
 
-    public void setPv(int pv) {
-        this.pv = pv;
-    }
+//    public void setPv(int pv) {
+//        this.pv = pv;
+//    }
 }
