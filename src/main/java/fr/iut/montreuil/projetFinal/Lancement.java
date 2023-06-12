b0ff35c2ae9365e088b1b4bf80da678afe0f0623
Vithushan <vvijayatharan@gmail.com>
2023-06-12 21:15:38 +0200

    Ajout défense Canon

--- src/main/java/fr/iut/montreuil/projetFinal/Lancement.java
+++ src/main/java/fr/iut/montreuil/projetFinal/Lancement.java
@@ -9,10 +9,10 @@ import java.io.IOException;
 
 public class Lancement extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Lancement.class.getResource("vue.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1550, 850);
-        stage.setTitle("Coc");
+        stage.setTitle("Clash of Clans Tower Defense");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/fr/iut/montreuil/projetFinal/modele/Canon.java
@@ -0,0 +1,8 @@
+package fr.iut.montreuil.projetFinal.modele;
+
+public class Canon extends Tour {
+
+    public Canon (double x,double y, Environnement env){
+        super("Canon",x,y,env,75,50,50);
+    }
+}
