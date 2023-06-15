05612fedd2af04a24048e9e861872c5718b9aa03
bsy44 <syboubacar444@gmail.com>
2023-06-15 23:01:11 +0200

    commit vue boulet et fleche

--- src/main/java/fr/iut/montreuil/projetFinal/vue/VueBoulet.java
+++ src/main/java/fr/iut/montreuil/projetFinal/vue/VueBoulet.java
@@ -1,10 +1,15 @@
 package fr.iut.montreuil.projetFinal.vue;
 
+import fr.iut.montreuil.projetFinal.modele.Ennemi;
 import fr.iut.montreuil.projetFinal.modele.Environnement;
 import fr.iut.montreuil.projetFinal.modele.Projectile;
+import javafx.animation.AnimationTimer;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 public class VueBoulet extends VueProjectile{
+
     public VueBoulet(Pane pane, Environnement env, Projectile p) {
         super(pane, env, p, "boulet.png");
     }
