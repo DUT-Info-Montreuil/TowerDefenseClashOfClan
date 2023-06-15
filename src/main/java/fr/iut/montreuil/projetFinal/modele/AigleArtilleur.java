cf748b655b52c5741d3a5e5c49248049fd19be5d
Vithushan <vvijayatharan@gmail.com>
2023-06-15 23:49:41 +0200

    Modification classe AigleArtilleur

--- src/main/java/fr/iut/montreuil/projetFinal/modele/AigleArtilleur.java
+++ src/main/java/fr/iut/montreuil/projetFinal/modele/AigleArtilleur.java
@@ -2,6 +2,11 @@ package fr.iut.montreuil.projetFinal.modele;
 
 public class AigleArtilleur extends Tour {
     public AigleArtilleur (double x,double y, Environnement env){
-        super("Aigle Artilleur",x,y,env,500,300,100,350);
+        super("Aigle Artilleur",x,y,env,500,300,100);
+    }
+
+    @Override
+    public void tir() {
+
     }
 }
