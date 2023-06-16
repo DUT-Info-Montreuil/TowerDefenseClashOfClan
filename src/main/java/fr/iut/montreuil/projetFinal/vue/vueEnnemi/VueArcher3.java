6f1b2384846baf26399c36a83d58ab8e23f1e0e2
emrebondy <gucluemre976@gmail.com>
2023-06-16 16:31:05 +0200

    améliorer image niveau troupe

--- src/main/java/fr/iut/montreuil/projetFinal/vue/vueEnnemi/VueArcher3.java
+++ src/main/java/fr/iut/montreuil/projetFinal/vue/vueEnnemi/VueArcher3.java
@@ -1,4 +1,4 @@
-package fr.iut.montreuil.projetFinal.vue;
+package fr.iut.montreuil.projetFinal.vue.vueEnnemi;
 
 import fr.iut.montreuil.projetFinal.modele.Ennemi;
 import javafx.scene.layout.Pane;
